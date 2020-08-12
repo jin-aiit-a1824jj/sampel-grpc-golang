5755bc93c17f89e5acbfbf840002cd16fb32a81d
a1824jj <a1824jj@aiit.ac.jp>
2020-08-12 09:39:07 +0000

    Sum API

---
+++ calculator/calculator_client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"../calculatorpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Calculator client")
+	cc, err := grpc.Dial("0.0.0.0:5000", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+	// fmt.Printf("Creted client:%f", c)
+
+	doUnary(c)
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a Sum Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		FirstNumber:  5,
+		SecondNumber: 40,
+	}
+	res, err := c.Sum(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Sum RPC: %v", err)
+	}
+	log.Printf("Response from Sum: %v", res.SumResult)
+}
