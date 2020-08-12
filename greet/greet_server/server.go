9936b99d19be07a268cc9e93f0e6506f3f50918f
a1824jj <a1824jj@aiit.ac.jp>
2020-08-12 04:11:20 +0000

    server setup Boilerplate code

---
+++ greet/greet_server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"../greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello World")
+
+	lis, err := net.Listen("tcp", "0.0.0.0:5000")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	greetpb.RegisterGreetServiceServer(s, &server{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
