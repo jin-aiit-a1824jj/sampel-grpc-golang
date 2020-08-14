9f916ae1fb6d6d6948cacbf512d650cb9dadea8c
a1824jj <a1824jj@aiit.ac.jp>
2020-08-14 08:22:46 +0000

    gRPC Reflection & Evans CLI

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -8,6 +8,8 @@ import (
 	"math"
 	"net"
 
+	"google.golang.org/grpc/reflection"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
@@ -128,6 +130,9 @@ func main() {
 
 	s := grpc.NewServer()
 	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	//Register reflection service on gRPC server
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
